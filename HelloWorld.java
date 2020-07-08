111f5824dc5acf69e8618ac3f2da37bc14271c3f
Sarunas Verbus <sarunas@verbus.lt>
2020-07-08 16:08:41 +0200

    atsakoje pridejau pavarde ir kreipinio galunes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Sarunas!");
+        System.out.println("Hello, Sarunai Verbau!");
         System.out.println("Labas, pasauli!");
     }
 }
