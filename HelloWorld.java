d6ae944bf6cc79d367197d082f13ab7d026b04f9
Šarūnas Verbus <67976949+verbusharas@users.noreply.github.com>
2020-07-08 16:21:45 +0200

    Issprendziau konflikta

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello, Sarunas Verbus!");
-=======
         System.out.println("Hello, Sarunai Verbau!");
->>>>>>> feature
         System.out.println("Labas, pasauli!");
     }
 }
